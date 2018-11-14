7d23ac7a5d11a4f0f4ba1703f83a8fb8ee734efb
makoto <1988makoto0901@gmail.com>
2018-11-15 00:51:02 +0900

    変数のスコープを細かくするよう修正、CSSの計算誤りを修正、カンマ後のインデントを調整

--- app/assets/javascripts/message.js
+++ app/assets/javascripts/message.js
@@ -1,7 +1,7 @@
 $(function() {
   function buildHTML(message){
-    var imageUrl = (message.image) ?`<img class="lower-message__image" src="${message.image}">` : "";
-    var html = `<div class="message">
+    let imageUrl = (message.image) ?`<img class="lower-message__image" src="${message.image}">` : "";
+    let html = `<div class="message">
                   <div class="upper-message">
                     <div class="upper-message__user-name">
                       ${message.name}
@@ -22,8 +22,8 @@ $(function() {
 
   $('#new_message').on('submit', function(e){
     e.preventDefault();
-    var formData = new FormData(this);
-    var url = $(this).attr('action');
+    let formData = new FormData(this);
+    let url = $(this).attr('action');
 
     $.ajax({
       url: url,
@@ -34,16 +34,16 @@ $(function() {
       contentType: false
     })
     .done(function(data){
-      var html = buildHTML(data);
+      let html = buildHTML(data);
       $('.messages').append(html)
-      $('.messages').animate({scrollTop: $('.messages')[0].scrollHeight},'fast');
+      $('.messages').animate({scrollTop: $('.messages')[0].scrollHeight}, 'fast');
       $("#new_message")[0].reset();
     })
     .fail(function(){
       alert('メッセージを入力してください');
     })
     .always(function(){
-      $('.form__send').prop('disabled',false)
+      $('.form__send').prop('disabled', false);
     })
   })
 });
